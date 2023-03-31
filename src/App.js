cba98d7a8f5325ed1a485ff7864a201d73ca249a
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-03-31 23:10:04 +0530

    Counter Range added.

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App(){
       </header>
       <h2>Current value of count is {count}</h2>
       <button onClick={() => setCount(0)}>Reset Counter</button>
-      <button onClick={() => setCount(count+1)}>Increase Counter</button>
-      <button onClick={() => setCount(count-1)}>Decrease Counter</button>
+      <button onClick={() => (count >= 10 ? "" : setCount(count+1))}>Increase Counter</button>
+      <button onClick={() => (count <= 0 ? "" : setCount(count-1))}>Decrease Counter</button>
     </div>
   )
 }
